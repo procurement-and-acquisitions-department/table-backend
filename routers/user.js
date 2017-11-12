a135b2a8bfd8a32352d7411f6ab2b392f32086d8
Kas Perch <the@nodebotani.st>
2017-11-12 13:29:30 -0600

    Added models, required attributes for User

--- routers/user.js
+++ routers/user.js
@@ -1,8 +1,24 @@
 const Express = require('express')
 let userRouter = Express.Router()
 
-userRouter.get('/list', (req, res) => {
+userRouter.get('/', (req, res) => {
   res.end('user list')
+})
+
+userRouter.get('/:userId', (req, res) => {
+  res.end('get specific user')
+})
+
+userRouter.post('/', (req, res) => {
+  res.end('create user')
+})
+
+userRouter.put('/:userId', (req, res) => {
+  res.end('update user')
+})
+
+userRouter.delete('/:userId', (req, res) => {
+  res.end('delete user')
 })
 
 module.exports = userRouter
--- schemas/user.js
+++ schemas/user.js
@@ -3,9 +3,23 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-  username: String,
-  name: String,
-  pronouns: Array,
+  ID: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  pronouns: {
+    active: String,
+    passive: String,
+    possessive: String
+  },
   Avatar: String,
   RFIDTag: String,
   password: String,
