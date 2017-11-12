774abe4263a4bd1528d080ef5ff621b53595e84b
Kas Perch <the@nodebotani.st>
2017-11-12 16:30:33 -0600

    Users can be added now

--- routers/user.js
+++ routers/user.js
@@ -7,18 +7,42 @@ let userRouter = Express.Router()
 userRouter.get('/', (req, res) => {
   User.find({}, (err, users) => {
     if (err) {
-      res.status(500).end(err.toString())
+      res.status(500).end(err)
     }
     res.status(200).end(JSON.stringify(users))
   })
 })
 
-userRouter.get('/:userId', (req, res) => {
-  res.end('get specific user')
+userRouter.get('/:username', (req, res) => {
+
 })
 
 userRouter.post('/', (req, res) => {
-  res.end('create user')
+  if (!req.body.username) {
+    res.status(400).end('username property required')
+  } else if (!req.body.name) {
+    res.status(400).end('name property required')
+  }
+
+  User.find({ username: req.body.username }, function (err, existingUser) {
+    if (err) {
+      res.status(500).end(err)
+    }
+    if (existingUser.length > 0) {
+      res.status(400).end('User ' + req.body.username + ' already exists')
+    }
+
+    let user = new User({
+      username: req.body.username,
+      name: req.body.name
+    })
+    user.save(function (err, user) {
+      if (err) {
+        res.status(500).end(err.toString())
+      }
+      res.status(201).end(JSON.stringify(user))
+    })
+  })
 })
 
 userRouter.put('/:userId', (req, res) => {
--- schemas/user.js
+++ schemas/user.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-  userID: String,
   username: {
     type: String,
     required: true
